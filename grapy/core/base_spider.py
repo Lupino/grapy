c97584aab59b9df7ec4ce265d682c0843fa877db
Lupino <lmjubuntu@gmail.com>
2017-11-28 17:51:32 +0800

    update start_request and remove start_urls

--- grapy/core/base_spider.py
+++ grapy/core/base_spider.py
@@ -4,22 +4,17 @@ class BaseSpider(object):
     '''The BaseSpider, all the spider recommend to extends this'''
 
     name = None
-    start_urls = []
 
-    def __init__(self, name=None, start_urls=[]):
+    def __init__(self, name=None):
         '''
         @name: the spider name, unique
-
-        @start_urls: the start request url
         '''
         if not self.name:
             self.name = name
-        if not self.start_urls:
-            self.start_urls = start_urls
 
     def start_request(self):
-        '''you must rewrite it for a start request'''
-        raise NotImplementedError('you must rewrite at sub class')
+        '''you mast rewrite it for a start request'''
+        return []
 
     def parse(self, response):
         '''
