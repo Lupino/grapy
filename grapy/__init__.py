3aa741cc714e5bb76f21e6d7ed84464d18368aca
Lupino <lmjubuntu@gmail.com>
2020-10-09 17:03:28 +0800

    export DropItem and IgnoreRequest

--- grapy/__init__.py
+++ grapy/__init__.py
@@ -1,7 +1,8 @@
-from .core import Engine, BaseSpider, Item
+from .core import Engine, BaseSpider, Item, DropItem, IgnoreRequest
 from .request import Request
 from .response import Response
 
-__all__ = ['Request', 'Response', 'BaseSpider', 'Item', 'engine']
+__all__ = ['Request', 'Response', 'BaseSpider', 'Item', 'engine', 'DropItem',
+        'IgnoreRequest']
 
 engine = Engine()
--- grapy/core/__init__.py
+++ grapy/core/__init__.py
@@ -2,7 +2,8 @@ from .engine import Engine
 from .base_spider import BaseSpider
 from .base_sched import BaseScheduler
 from .base_request import BaseRequest
+from .exceptions import DropItem, IgnoreRequest
 from .item import Item, dump_item, load_item
 
-__all__ = ['Engine', 'BaseSpider', 'BaseScheduler', 'BaseRequest',
-        'Item', 'dump_item', 'load_item']
+__all__ = ['Engine', 'BaseSpider', 'BaseScheduler', 'BaseRequest', 'DropItem',
+        'IgnoreRequest', 'Item', 'dump_item', 'load_item']
