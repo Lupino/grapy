734907ef6e5ae08c0cb3392a8335c873753f2915
Lupino <lmjubuntu@gmail.com>
2020-10-10 09:36:08 +0800

    add make_spider decorator

--- grapy/utils.py
+++ grapy/utils.py
@@ -2,10 +2,11 @@ from importlib import import_module as _import_module
 import logging
 import glob
 import os.path
+from .core.base_spider import BaseSpider
 
 __all__ = [
     'import_module', 'logger', 'import_spiders', 'middleware',
-    'before_push_request', 'before_request', 'after_response'
+    'before_push_request', 'before_request', 'after_response', 'make_spider'
 ]
 
 logger = logging.getLogger('grapy')
@@ -56,20 +57,17 @@ def import_spiders(spider_path,
             continue
 
         for cls_name in dir(module):
-            if not cls_name.endswith('Spider'):
-                continue
-
             if cls_name in ignore_cls_names:
                 continue
 
             spiderclass = getattr(module, cls_name, None)
-            if spiderclass:
+            if isinstance(spiderclass, BaseSpider):
+                spiders.append(spiderclass)
+            elif cls_name.endswith('Spider'):
                 spider = spiderclass()
                 if spider.name is None:
                     spider.name = cls_name[:-6]
                 spiders.append(spider)
-            else:
-                logging.error('{}.{} invalid.'.format(module_path, cls_name))
 
     return spiders
 
@@ -79,14 +77,36 @@ class Middleware(object):
 
 
 def middleware(name):
-    def _dref(func):
-        middleware = Middleware()
-        setattr(middleware, name, func)
-        return middleware
+    def _middleware(func):
+        m = Middleware()
+        setattr(m, name, func)
+        return m
 
-    return _dref
+    return _middleware
 
 
 before_push_request = middleware('before_push_request')
 before_request = middleware('before_request')
 after_request = middleware('after_request')
+
+
+def make_spider(func_name=None, start_urls=[]):
+    '''Make a spider
+    @param func_name the spider name
+    @param start_urls Request list
+    '''
+    def _spider(func):
+        name = func_name
+        if name is None:
+            name = func.__name__
+        s = BaseSpider(name)
+        s.parse = func
+
+        def start_request():
+            return start_urls
+
+        s.start_request = start_request
+
+        return s
+
+    return _spider
