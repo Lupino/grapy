1342e7ba53f56e7df9019e5650f0525783c5917c
Lupino <lmjubuntu@gmail.com>
2022-05-14 21:25:30 +0800

    add PeriodicScheduler

--- grapy/sched.py
+++ grapy/sched.py
@@ -1,7 +1,9 @@
 from .core import BaseScheduler
 from .utils import logger
 from .core.exceptions import IgnoreRequest, RetryRequest
+from .core.item import dump_item, load_item
 from asyncio_pool import AioPool
+from .request import Request
 
 __all__ = ['Scheduler']
 
@@ -27,3 +29,38 @@ class Scheduler(BaseScheduler):
 
     async def join(self):
         await self._pool.join()
+
+
+class PeriodicScheduler(BaseScheduler):
+    async def push_req(self, req):
+        key = req.get_hash()
+        await self._worker.submit_job('submit_req', key, bytes(req))
+
+    async def submit_req(self, job):
+        req = Request.build(job.workload)
+        try:
+            await BaseScheduler.submit_req(self, req)
+        except IgnoreRequest:
+            pass
+        except RetryRequest:
+            req.unique = False
+            return await job.sched_later(job.payload.count + 10, 1)
+        except Exception as e:
+            logger.exception(e)
+
+        await job.done()
+
+    async def push_item(self, item):
+        key = item.get_hash()
+        await self._worker.submit_job('submit_item', key, dump_item(item))
+
+    async def submit_item(self, job):
+        item = load_item(job.workload)
+        await BaseScheduler.submit_item(self, item)
+        await job.done()
+
+    async def init(self, worker, submit_item=True):
+        self._worker = worker
+        await self._worker.add_func('submit_req', self.submit_req)
+        if submit_item:
+            await self._worker.add_func('submit_item', self.submit_item)
